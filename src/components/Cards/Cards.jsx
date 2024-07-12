261cd9c22c38952378bc97030ce8ec70dc95724d
J[A]shy <jekpadt@gmail.com>
2024-07-12 18:26:39 +0300

    Fix superpower collision

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -99,7 +99,7 @@ export function Cards({ pairsCount = 3, previewSeconds = 1, lives = 1 }) {
 
     setTimeout(() => {
       setGameStartDate(current => new Date(current.getTime() + 5000));
-      displayedCards.current = cards;
+      setCards(cards => cards); // Т.к. карточки могут измениться за это время
       setStatus(STATUS_IN_PROGRESS);
     }, 5000);
   };
